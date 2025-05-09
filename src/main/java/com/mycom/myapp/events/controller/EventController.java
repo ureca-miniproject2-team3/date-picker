a583273b258411b2de2395490ab200162646d933
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-05-09 17:47:39 +0900

    feat : EventController 이벤트 삭제 메서드 구현

--- src/main/java/com/mycom/myapp/events/controller/EventController.java
+++ src/main/java/com/mycom/myapp/events/controller/EventController.java
@@ -8,6 +8,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +41,19 @@ public class EventController {
     @Operation(summary = "이벤트 수정", description = "이벤트 제목을 수정하고, 날짜를 추가할 수 있습니다.")
     public ResponseEntity<EventResultDto> updateEvent(EventDto eventDto) {
         EventResultDto result = eventService.updateEvent(eventDto);
+
+        if (result.getResult().equals("success")) {
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
+        }
+    }
+
+    @DeleteMapping("/events/{eventId}")
+    @Operation(summary = "이벤트 삭제", description = "이벤트를 삭제합니다.")
+    public ResponseEntity<EventResultDto> deleteEvent(@PathVariable Long eventId) {
+        EventResultDto result = eventService.deleteEvent(eventId);
 
         if (result.getResult().equals("success")) {
             return ResponseEntity.status(HttpStatus.OK).body(result);
