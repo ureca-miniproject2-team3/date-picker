7503eabb46e6bd81ed0dfdf6c223db426f3a59bd
Jang Seung Beom <wkdtmf357@gmail.com>
2025-05-09 15:48:11 +0900

    feat: 회원가입 UserController 구현

---
+++ src/main/java/com/mycom/myapp/users/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.mycom.myapp.users.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycom.myapp.users.dto.UserDto;
+import com.mycom.myapp.users.dto.UserResultDto;
+import com.mycom.myapp.users.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+	
+	private final UserService userService;
+	
+	@PostMapping("/users/register")
+	public ResponseEntity<UserResultDto> registerUser(UserDto userDto) {
+		UserResultDto userResultDto = userService.registerUser(userDto);
+		
+		if("success".equals(userResultDto.getResult())) {
+			return ResponseEntity.ok(userResultDto);
+		} 
+		else if("exist".equals(userResultDto.getResult())) {
+			return ResponseEntity.badRequest().body(userResultDto);
+		}
+		else {
+			return ResponseEntity.internalServerError().body(userResultDto);
+		}
+	}
+	
+	
+}
