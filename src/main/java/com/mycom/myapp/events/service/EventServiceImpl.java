cf17177e973c9f10624c58268c38fce83f1574a1
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-05-09 14:04:48 +0900

    feat : EventService 이벤트 수정 관련 메서드 구현

--- src/main/java/com/mycom/myapp/events/service/EventServiceImpl.java
+++ src/main/java/com/mycom/myapp/events/service/EventServiceImpl.java
@@ -5,6 +5,7 @@ import com.mycom.myapp.events.dto.EventDto;
 import com.mycom.myapp.events.dto.EventResultDto;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
@@ -38,6 +39,37 @@ public class EventServiceImpl implements EventService {
 
         } catch (Exception e) {
             log.warn("이벤트 생성 중 예외 발생: {}", e.getMessage());
+            result.setResult("fail");
+            throw e;
+        }
+
+        return result;
+    }
+
+    @Override
+    @Transactional
+    public EventResultDto updateEvent(EventDto eventDto) {
+        EventResultDto result = new EventResultDto();
+
+        try {
+            eventDao.updateEventTitle(eventDto);
+
+            List<LocalDate> existingDates = eventDao.getExistingDates(eventDto.getEventId());
+
+            // 새롭게 추가되는 날짜만 필터링
+            List<LocalDate> newDates = eventDto.getEventDates();
+            List<LocalDate> datesToInsert = newDates.stream()
+                    .filter(date -> !existingDates.contains(date))
+                    .toList();
+
+            for (LocalDate date : datesToInsert) {
+                eventDao.insertEventDate(eventDto.getEventId(), date);
+            }
+
+            result.setResult("success");
+
+        } catch (Exception e) {
+            log.warn("이벤트 수정 중 예외 발생: {}", e.getMessage());
             result.setResult("fail");
             throw e;
         }
