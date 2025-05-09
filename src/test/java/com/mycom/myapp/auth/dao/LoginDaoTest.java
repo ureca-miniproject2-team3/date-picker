c7f0a837526ada9fd5b76ca6500f166c4f496721
Jang Seung Beom <wkdtmf357@gmail.com>
2025-05-09 10:25:19 +0900

    feat: Login Dao 테스트 구현

---
+++ src/test/java/com/mycom/myapp/auth/dao/LoginDaoTest.java
@@ -0,0 +1,29 @@
+package com.mycom.myapp.auth.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mycom.myapp.users.dto.UserDto;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class LoginDaoTest {
+
+	@Autowired
+	private LoginDao loginDao;
+	
+	@Test
+	public void findByEmailTest() {
+		UserDto userDto = loginDao.findByEmail("hong@gildong.com");
+		
+		assertEquals(1, userDto.getId());
+		assertEquals("hong@gildong.com", userDto.getEmail());
+		assertEquals("홍길동", userDto.getName());
+	}
+}
