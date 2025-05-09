ef68ad8b21aeff860887148717512cc008ca7d18
Jang Seung Beom <wkdtmf357@gmail.com>
2025-05-09 14:54:09 +0900

    test: 회원가입 Service Layer 테스트 완료

---
+++ src/test/java/com/mycom/myapp/users/service/UserServiceTest.java
@@ -0,0 +1,82 @@
+package com.mycom.myapp.users.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.mycom.myapp.auth.dao.LoginDao;
+import com.mycom.myapp.users.dao.UserDao;
+import com.mycom.myapp.users.dto.UserDto;
+import com.mycom.myapp.users.dto.UserResultDto;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+	@InjectMocks
+	private UserServiceImpl userService;
+	
+	@Mock
+	private UserDao userDao;
+	
+	@Mock 
+	private LoginDao loginDao;
+	
+	@Mock
+	private PasswordEncoder passwordEncoder; 
+	
+	@Test
+	public void registerUserTest_Exist() {
+		UserDto user1 = UserDto.builder()
+					.name("aaa")
+					.email("aaa@aaa.com")
+					.password("aaa")
+					.build();
+		
+		when(loginDao.findByEmail("aaa@aaa.com")).thenReturn(UserDto.builder().
+				id(1).
+				name("bbb").
+				email("aaa@aaa.com").
+				password("aaa").
+				build());
+		
+		UserResultDto userResultDto = userService.registerUser(user1);
+		
+		assertEquals("exist", userResultDto.getResult());
+	}
+	
+	@Test
+	public void registerUserTest_Success() {
+		UserDto userDto = UserDto.builder()
+				.name("aaa")
+				.email("aaa@aaa.com")
+				.password("aaa")
+				.build();
+		
+		UserResultDto userResultDto = userService.registerUser(userDto);
+		
+		assertEquals("success", userResultDto.getResult());
+	}
+	
+    @Test
+    public void registerUserTest_Fail() {
+        UserDto userDto = UserDto.builder()
+                .name("aaa")
+                .email("aaa@aaa.com")
+                .password("aaa")
+                .build();
+
+        when(loginDao.findByEmail(anyString()))
+                .thenThrow(new RuntimeException("DB error"));
+
+        UserResultDto result = userService.registerUser(userDto);
+        assertEquals("fail", result.getResult());
+    }
+}
+
