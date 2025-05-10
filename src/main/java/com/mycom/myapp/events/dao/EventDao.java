831ed3bd4f4b21a2e198e470dd4eddbb03f34bb2
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-05-10 17:21:18 +0900

    feat : mapper, dao 레이어 이벤트 상세 조회 기능 구현

--- src/main/java/com/mycom/myapp/events/dao/EventDao.java
+++ src/main/java/com/mycom/myapp/events/dao/EventDao.java
@@ -3,7 +3,6 @@ package com.mycom.myapp.events.dao;
 import com.mycom.myapp.events.dto.EventDto;
 import com.mycom.myapp.events.dto.EventSummaryDto;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -13,6 +12,11 @@ public interface EventDao {
 
     // 이벤트 리스트 조회
     List<EventSummaryDto> listEvent(@Param("userId") Long userId);
+
+    // 이벤트 상세 정보 조회
+    EventDto detailEvent(@Param("eventId") Long eventId);
+    List<Long> findUserIdsByEventId(@Param("eventId") Long eventId);
+    List<LocalDate> findEventDatesByEventId(@Param("eventId") Long eventId);
 
     // 이벤트 등록
     void insertEvent(EventDto eventDto);
