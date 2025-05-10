96f521724fe5c969b23655d875227b2377649304
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-05-10 22:38:18 +0900

    fix : EventDto 기본 생성자 및 전체 필드 생성자 추가

--- src/main/java/com/mycom/myapp/events/dto/EventDto.java
+++ src/main/java/com/mycom/myapp/events/dto/EventDto.java
@@ -2,11 +2,15 @@ package com.mycom.myapp.events.dto;
 
 import java.time.LocalDate;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventDto {
 
     private Long eventId;
