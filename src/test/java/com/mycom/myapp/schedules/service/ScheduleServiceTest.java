be0241e4ffb967e7e7f185ac63990ba8d3d07191
Jang Seung Beom <wkdtmf357@gmail.com>
2025-05-11 21:30:04 +0900

    test: Schedule 목록, 상세 조회 기능 Service Layer 테스트 완료

--- src/test/java/com/mycom/myapp/schedules/service/ScheduleServiceTest.java
+++ src/test/java/com/mycom/myapp/schedules/service/ScheduleServiceTest.java
@@ -1,9 +1,13 @@
 package com.mycom.myapp.schedules.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -71,5 +75,71 @@ public class ScheduleServiceTest {
 		ScheduleResultDto result = scheduleService.insertSchedule(scheduleDto);
 		
 		assertEquals("fail", result.getResult());
+	}
+	
+	@Test
+	void listScheduleTest_Success() {
+		ScheduleDto schedule1 = ScheduleDto.builder()
+						.scheduleId(1L)
+						.userId(1L)
+						.eventId(205L)
+						.startTime(LocalDateTime.of(2025, 5, 15, 13, 00, 00))
+						.endTime(LocalDateTime.of(2025, 5, 15, 15, 00, 00))
+						.build();
+		
+		ScheduleDto schedule2 = ScheduleDto.builder()
+				.scheduleId(2L)
+				.userId(1L)
+				.eventId(205L)
+				.startTime(LocalDateTime.of(2025, 5, 16, 13, 00, 00))
+				.endTime(LocalDateTime.of(2025, 5, 16, 15, 00, 00))
+				.build();
+		
+		when(scheduleDao.listSchedule(any(Long.class))).thenReturn(List.of(schedule1, schedule2));
+		
+		ScheduleResultDto result = scheduleService.listSchedule(205L);
+		
+		assertEquals("success", result.getResult());
+		assertIterableEquals(List.of(schedule1, schedule2), result.getScheduleDtoList());
+	}
+	
+	@Test
+	void listScheduleTest_Fail() {
+		when(scheduleDao.listSchedule(any(Long.class)))
+				.thenThrow(new RuntimeException("DB Error"));
+		
+		ScheduleResultDto result = scheduleService.listSchedule(Long.MAX_VALUE);
+		
+		assertEquals("fail", result.getResult());
+		assertNull(result.getScheduleDtoList());
+	}
+	
+	@Test
+	void detailScheduleTest_Success() {
+		ScheduleDto schedule = ScheduleDto.builder()
+						.scheduleId(1L)
+						.userId(1L)
+						.eventId(205L)
+						.startTime(LocalDateTime.of(2025, 5, 15, 13, 00, 00))
+						.endTime(LocalDateTime.of(2025, 5, 15, 15, 00, 00))
+						.build();
+		
+		when(scheduleDao.detailSchedule(1L)).thenReturn(schedule);
+		
+		ScheduleResultDto result = scheduleService.detailSchedule(1L);
+		
+		assertEquals("success", result.getResult());
+		assertEquals(schedule, result.getScheduleDto());
+	}
+	
+	@Test
+	void detailScheduleTest_Fail() {
+		when(scheduleDao.detailSchedule(any(Long.class)))
+				.thenThrow(new RuntimeException("DB Error"));
+		
+		ScheduleResultDto result = scheduleService.detailSchedule(Long.MAX_VALUE);
+		
+		assertEquals("fail", result.getResult());
+		assertNull(result.getScheduleDto());
 	}
 }
