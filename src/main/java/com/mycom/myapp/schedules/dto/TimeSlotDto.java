392577bd40284d45300b51ce7dad934cdcbb1a3d
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-05-11 22:56:21 +0900

    feat : TimeSlotDto 에 특정 시간대에 참여 가능한 사용자 ID 리스트 필드 추가

--- src/main/java/com/mycom/myapp/schedules/dto/TimeSlotDto.java
+++ src/main/java/com/mycom/myapp/schedules/dto/TimeSlotDto.java
@@ -1,11 +1,15 @@
 package com.mycom.myapp.schedules.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TimeSlotDto {
 
     private LocalDateTime start;
     private LocalDateTime end;
+    private List<Long> userIds;
 }
