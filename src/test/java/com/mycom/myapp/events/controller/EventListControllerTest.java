af277040473e7b3c150b2672fbe9c8c251a85cdb
Jang Seung Beom <wkdtmf357@gmail.com>
2025-05-14 16:56:51 +0900

    test: List, Detail에서 Timeline 불러오는 기능 Controller Layer 테스트 완료

--- src/test/java/com/mycom/myapp/events/controller/EventListControllerTest.java
+++ src/test/java/com/mycom/myapp/events/controller/EventListControllerTest.java
@@ -8,8 +8,11 @@ import static org.mockito.Mockito.when;
 
 import com.mycom.myapp.events.dto.EventResultDto;
 import com.mycom.myapp.events.dto.EventSummaryDto;
+import com.mycom.myapp.events.dto.TimelineDto;
 import com.mycom.myapp.events.dto.type.EventStatus;
 import com.mycom.myapp.events.service.EventService;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -34,6 +37,12 @@ public class EventListControllerTest {
         Long userId = 1L;
         EventResultDto resultDto = new EventResultDto();
         resultDto.setResult("success");
+        TimelineDto timeline = TimelineDto.builder()
+        		.timelineId(100L)
+        		.eventId(2L)
+        		.startTime(LocalDateTime.of(2025, 5, 1, 13, 00, 00))
+        		.endTime(LocalDateTime.of(2025, 5, 1, 16, 00, 00))
+        		.build();
         
         List<EventSummaryDto> eventList = new ArrayList<>();
         EventSummaryDto event1 = new EventSummaryDto();
@@ -41,7 +50,14 @@ public class EventListControllerTest {
         event1.setTitle("테스트 이벤트1");
         event1.setStatus(EventStatus.EXPIRED);
         
+        EventSummaryDto event2 = new EventSummaryDto();
+        event2.setEventId(2L);
+        event2.setTitle("테스트 이벤트2");
+        event2.setStatus(EventStatus.COMPLETED);
+        event2.setTimeline(timeline);
+        
         eventList.add(event1);
+        eventList.add(event2);
         
         resultDto.setEventDtoList(eventList);
 
