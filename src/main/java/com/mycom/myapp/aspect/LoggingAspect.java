09673b6d13750d0bf269cb55d6cf47102e390c7d
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-05-14 16:41:57 +0900

    feat: GET, PUT, POST, DELETE 메서드 로깅 구현

---
+++ src/main/java/com/mycom/myapp/aspect/LoggingAspect.java
@@ -0,0 +1,149 @@
+package com.mycom.myapp.aspect;
+
+import com.mycom.myapp.config.LoginUserDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    // Pointcut
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    private void getPointcut() {
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PutMapping)")
+    private void putPointcut() {
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+    private void postPointcut() {
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+    private void deletePointcut() {
+    }
+
+    // Join Point
+    @After("getPointcut()")
+    public void logGetMapping(JoinPoint joinPoint) {
+        Authentication auth = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated() || auth instanceof AnonymousAuthenticationToken) {
+            return;
+        }
+
+        String username;
+        Long userId = null;
+
+        Object principal = auth.getPrincipal();
+
+        if (principal instanceof LoginUserDetails) {
+            LoginUserDetails user = (LoginUserDetails) principal;
+            username = user.getUsername();
+            userId = user.getUserId();
+
+        } else {
+            username = auth.getName(); // 기본 UserDetails or 다른 Principal 로 처리
+        }
+
+        String methodName = joinPoint.getSignature().getName();
+
+        log.info("[GET] 사용자 [ id={} / {} ] 이 [ {} ] 을 실행했습니다.", userId, username, methodName);
+    }
+
+    @After("putPointcut()")
+    public void logPutMapping(JoinPoint joinPoint) {
+        Authentication auth = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated() || auth instanceof AnonymousAuthenticationToken) {
+            return;
+        }
+
+        String username;
+        Long userId = null;
+
+        Object principal = auth.getPrincipal();
+
+        if (principal instanceof LoginUserDetails user) {
+            username = user.getUsername();
+            userId = user.getUserId();
+
+        } else {
+            username = auth.getName();
+        }
+
+        String methodName = joinPoint.getSignature().getName();
+
+        log.info("[PUT] 사용자 [ id={} / {} ] 이 [ {} ] 을 실행했습니다.", userId, username, methodName);
+    }
+
+    @After("postPointcut()")
+    public void logPostMapping(JoinPoint joinPoint) {
+        Authentication auth = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated() || auth instanceof AnonymousAuthenticationToken) {
+            return;
+        }
+
+        String username;
+        Long userId = null;
+
+        Object principal = auth.getPrincipal();
+
+        if (principal instanceof LoginUserDetails user) {
+            username = user.getUsername();
+            userId = user.getUserId();
+
+        } else {
+            username = auth.getName();
+        }
+
+        String methodName = joinPoint.getSignature().getName();
+
+        log.info("[POST] 사용자 [ id={} / {} ] 이 [ {} ] 을 실행했습니다.", userId, username, methodName);
+    }
+
+    @After("deletePointcut()")
+    public void logDeleteMapping(JoinPoint joinPoint) {
+        Authentication auth = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated() || auth instanceof AnonymousAuthenticationToken) {
+            return;
+        }
+
+        String username;
+        Long userId = null;
+
+        Object principal = auth.getPrincipal();
+
+        if (principal instanceof LoginUserDetails user) {
+            username = user.getUsername();
+            userId = user.getUserId();
+
+        } else {
+            username = auth.getName();
+        }
+
+        String methodName = joinPoint.getSignature().getName();
+
+        log.info("[DELETE] 사용자 [ id={} / {} ] 이 [ {} ] 을 실행했습니다.", userId, username, methodName);
+    }
+}
