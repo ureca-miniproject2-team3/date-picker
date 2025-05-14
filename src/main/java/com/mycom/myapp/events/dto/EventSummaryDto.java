5bebf5007df360856e46934d1d9dddc4e2ad0478
Jang Seung Beom <wkdtmf357@gmail.com>
2025-05-14 11:25:37 +0900

    feat: EventDto, EventSummaryDto 상태 체크 Status 필드 추가

--- src/main/java/com/mycom/myapp/events/dto/EventSummaryDto.java
+++ src/main/java/com/mycom/myapp/events/dto/EventSummaryDto.java
@@ -3,10 +3,13 @@ package com.mycom.myapp.events.dto;
 import lombok.Data;
 import java.util.List;
 
+import com.mycom.myapp.events.dto.type.EventStatus;
+
 @Data
 public class EventSummaryDto {
     private Long eventId;
     private String title;
     private List<Long> userIds;
     private List<String> userNames;
+    private EventStatus status;
 }
