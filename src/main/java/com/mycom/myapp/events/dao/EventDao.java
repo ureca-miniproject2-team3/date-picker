d8b7fc90de96d035cd36f23b2d69bb6a4f8b3a58
Jang Seung Beom <wkdtmf357@gmail.com>
2025-05-15 15:06:13 +0900

    feat: User_event의 is_sent 필드 0 -> 1 (false -> true) 수정 기능 DAO Layer 구현

--- src/main/java/com/mycom/myapp/events/dao/EventDao.java
+++ src/main/java/com/mycom/myapp/events/dao/EventDao.java
@@ -49,5 +49,8 @@ public interface EventDao {
     void insertTimeline(TimelineDto timelineDto);
 
     // is_sent = false (알림 미수신)인 user_event 조회
-    List<UserEventDto> listUnsentUserEvent(Long userId);
+    List<UserEventDto> listUnsentUserEvent(@Param("userId") Long userId);
+    
+    // is_sent = false인 user_event를 true로 변경
+    void updateUnsentUserEvent(@Param("userId") Long userId, @Param("eventId") Long eventId);
 }
